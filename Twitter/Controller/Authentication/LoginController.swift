6e2a124cf042d82916b75c97ba263e2a717799e3
ArturDokhno <artur.dohno@icloud.com>
2022-06-30 12:02:31 +0500

    Creating Input Container View

--- Twitter/Controller/Authentication/LoginController.swift
+++ Twitter/Controller/Authentication/LoginController.swift
@@ -12,11 +12,43 @@ class LoginController: UIViewController {
     // MARK: - Properties
     
     private let logoImageView: UIImageView = {
-        let image = UIImageView()
-        image.contentMode = .scaleAspectFit
-        image.clipsToBounds = true
-        image.image = UIImage(named: "TwitterLogo")
-        return image
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "TwitterLogo")
+        return imageView
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPurple
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "mail_outline_white")
+        
+        view.addSubview(imageView)
+        imageView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor,
+                         paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "lock_outline_white")
+        
+        view.addSubview(imageView)
+        imageView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor,
+                         paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+        
+        return view
     }()
     
     // MARK: - Lifecycle
@@ -41,6 +73,13 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
     
 }
