0c7ab9bc3020910eed73bf134b8c6ccdb71f9c83
ArturDokhno <artur.dohno@icloud.com>
2022-07-08 14:49:03 +0500

    Fetch Tweets From Firebase & Create Tweet Object

--- Twitter/Controller/FeedController.swift
+++ Twitter/Controller/FeedController.swift
@@ -24,6 +24,15 @@ class FeedController: UIViewController {
         super.viewDidLoad()
 
         configureUI()
+        fetchTweets()
+    }
+    
+    // MARK: - API
+    
+    func fetchTweets() {
+        TweetService.shared.fetchTweets { tweets in
+            print("DEBUG: Tweets re \(tweets)")
+        }
     }
     
     // MARK: - Helpers
