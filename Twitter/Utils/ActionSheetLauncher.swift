0dea9523920695a01f8c0a6393bc06d8ed965715
ArturDokhno <artur.dohno@icloud.com>
2022-07-23 17:15:34 +0500

    Getting Started

---
+++ Twitter/Utils/ActionSheetLauncher.swift
@@ -0,0 +1,26 @@
+//
+//  ActionSheetLauncher.swift
+//  Twitter
+//
+//  Created by Артур Дохно on 23.07.2022.
+//
+
+import Foundation
+
+class ActionSheetLauncher: NSObject {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    
+    // MARK: - Helpers
+    
+    func show() {
+        print("DEBUG: Show action sheet for user \(user.username)")
+    }
+}
